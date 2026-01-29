f2a3efbe198fc6f1f6535db0db2f357daf785c54
onifadjosh <onifadjosh@gmail.com>
2026-01-29 11:02:51 +0100

    Added zod validation and updated user registration controller and model

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,19 +1,34 @@
 const UserModel = require("../models/user.model");
+// const {z} = require("zod")
+
+// const registerUserSchema = z.object({
+//     name: z.string({required_error:"name is required"}).min(3),
+//     username: z.string({required_error:"username is required"}).min(3),
+//     email: z.string({required_error:"email is required"}).email("ivalid email format"),
+//     password: z.string({required_error:"password is required"}).min(3),
+  
+// }).strict()
 
 const registerUser=async(req, res)=>{
     const {name, username, password, email} = req.body
+    // const registerUserr= registerUserSchema.parse(req.body)
     try {
        let user = await UserModel.create(req.body)
        res.status(201).send(
         {
             message:"user created successfully",
             data:{
-                
+              name,
+               username,
+               email,
+               role:user?.role
             }
         }
        )
     } catch (error) {
-        
+        res.status(400).send({
+            message:"error creating user"
+        })
     }
 }
 
--- models/user.model.js
+++ models/user.model.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     default: "user",
   },
 //   lastLogin:
-}, {timestamps:true});
+}, {timestamps:true, strict:"throw"});
 
 const UserModel = mongoose.model("users", UserSchema);
 
