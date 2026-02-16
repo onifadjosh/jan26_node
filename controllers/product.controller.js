5445df31995f5286b6266f450d0a60d417f8115b
onifadjosh <onifadjosh@gmail.com>
2026-02-16 12:06:55 +0100

    Added cloudinary configuration and image upload functionality to product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,12 @@
 const ProductModel = require("../models/product.model");
+const UserModel = require("../models/user.model");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
 
 const listProduct = async (req, res) => {
   const {
@@ -8,10 +16,29 @@ const listProduct = async (req, res) => {
     productImage,
     productDescription,
   } = req.body;
- 
 
   try {
-    const product = await ProductModel.create({...req.body, createdBy:req.user});
+    const image = await cloudinary.uploader
+      .upload(productImage)
+      .then((result) => {
+        return result.secure_url;
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+
+    const product = await ProductModel.create({
+    
+        productName,
+        productPrice,
+        productQuantity,
+        productDescription,
+        productImage: image,
+
+      createdBy: req.user,
+    });
+    console.log("User ID being saved:", req.user);
 
     res.status(201).send({
       message: "Product created successfully",
@@ -29,58 +56,63 @@ const getProducts = async (req, res) => {
   // res.render("product", { products });
   try {
     console.log("hello");
-    const products = await ProductModel.find()
-    .populate("createdBy", "name email username _id")
+    const products = await ProductModel.find().populate(
+      "createdBy",
+      "name email username _id"
+    );
     // const products = await ProductModel.find().select("-productName")
+    console.log("First Product Creator ID:", products[0]?.createdBy);
+
     res.status(200).send({
       message: "product fetched successfully",
       products,
     });
   } catch (error) {
     console.log(error);
-    
+
     res.status(404).send({ message: "products not found" });
   }
 };
 
+const getProductsBy = async (req, res) => {
+  const { productName, productPrice, createdBy } = req.query;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
 
-const getProductsBy=async(req, res)=>{
-  const{productName, productPrice, createdBy}= req.query
-  const page = parseInt(req.query.page)||1
-  const limit= parseInt(req.query.limit)||10
-
-  const skip = (page-1)*limit
+  const skip = (page - 1) * limit;
 
   try {
-    const filter ={}
-  if(productName) filter.productName={$regex:productName, $options:"i"}
-  if(createdBy) filter.createdBy= createdBy
-  if(productPrice) filter.productPrice= productPrice
+    const filter = {};
+    if (productName)
+      filter.productName = { $regex: productName, $options: "i" };
+    if (createdBy) filter.createdBy = createdBy;
+    if (productPrice) filter.productPrice = productPrice;
 
-  const products = await ProductModel.find(filter).populate("createdBy", "name email username")
-  .skip(skip)
-  .limit(limit)
-  .sort({createdAt:-1})
+    const products = await ProductModel.find(filter)
+      .populate("createdBy", "name email username _id")
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
 
+    console.log("First Product Creator ID:", products[0]?.createdBy);
 
-  const total = await ProductModel.countDocuments()
-  res.status(200).send({
-    message: "product fetched successfully",
-    products,
+    const total = await ProductModel.countDocuments(filter);
+    res.status(200).send({
+      message: "product fetched successfully",
+      products,
 
-    meta:{
-      currentPage: page,
-      totalPages: Math.ceil(total / limit),
-      total
-    }
-  });
-
+      meta: {
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+        total,
+      },
+    });
   } catch (error) {
     console.log(error);
-    
+
     res.status(404).send({ message: "products not found" });
   }
-  }
+};
 
 const deleteProducts = async (req, res) => {
   const { id } = req.params;
@@ -106,23 +138,20 @@ const editProduct = async (req, res) => {
       ...(productName && { productName }),
       ...(productPrice && { productPrice }),
     };
-   
-      let product = await ProductModel.findByIdAndUpdate(
-        id,
-        allowedupdate,
-        { new: true }
-      );
-      res.status(200).send({
-        message: "product updated successfully",
-        product,
-      });
- 
-      if(!product){
-        res.status(404).send({
-          message: "product not found",
-        });
-      }
 
+    let product = await ProductModel.findByIdAndUpdate(id, allowedupdate, {
+      new: true,
+    });
+    res.status(200).send({
+      message: "product updated successfully",
+      product,
+    });
+
+    if (!product) {
+      res.status(404).send({
+        message: "product not found",
+      });
+    }
   } catch (error) {
     res.status(400).send({
       message: "product cannot be updated",
@@ -135,5 +164,5 @@ module.exports = {
   getProducts,
   deleteProducts,
   editProduct,
-  getProductsBy
+  getProductsBy,
 };
